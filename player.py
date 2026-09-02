a3527461006bfbbad28cde79dcc44bc4642a5057
Arthi Halder <arthihalder@gmail.com>
2026-09-02 09:17:04 +0600

    second commit

--- player.py
+++ player.py
@@ -3,37 +3,94 @@ import threading
 import time
 import msvcrt 
 import shutil
+import os
+
+os.system("cls")
 
 pygame.mixer.init()
 
 pygame.mixer.music.load("music/song.mp3")
 
+song_length = pygame.mixer.Sound("music/song.mp3").get_length()
+
+
+
+
 playing = False
 paused = False
-current_lyric = ""
+song_name = "LEVEL FIVE - TUMI"
+current_lyric = "♪ Current lyric ♪"
+current_time = 0
+total_time = 0
 
 WIDTH = shutil.get_terminal_size().columns
 
+def format_time(seconds):
+    minutes = int(seconds //60)
+    seconds = int(seconds % 60)
+    return f"{minutes:02d}:{seconds:02d}"
+
+total_time = format_time(song_length)
+
+Lyrics = []
+
+with open("lyrics/song.lrc", encoding="utf-8") as file:
+    for line in file:
+        time_stamp, lyric = line.strip().split("]", 1)
+        time_stamp = time_stamp[1:]
+
+        minutes, seconds = time_stamp.split(":")
+        lyric_time = int(minutes) * 60 + float(seconds)
+
+        Lyrics.append((lyric_time, lyric))
+
+
+print("\033[2J\033[H", end="")
+def draw_ui():
+
+    print(r"""
+       ____  _____________.__
+  ____ \   \/  /\______   \  | _____  ___.__. ___________
+_/ __ \ \     /  |     ___/  | \__  \<   |  |/ __ \_  __ \
+\  ___/ /     \  |    |   |  |__/ __ \\___  \  ___/|  | \/
+ \___  >___/\  \ |____|   |____(____  / ____|\___  >__|
+     \/      \_/                    \/\/         \/
+""")
+
+    print(song_name.center(WIDTH))
+    print()
+
+    print(current_lyric.center(WIDTH))
+    print()
+
+    print("progress_bar".center(WIDTH))
+    print()
+
+    print(f"{current_time} / {total_time}".center(WIDTH))
+    print()
+
+    print("[ P ] Play".center(WIDTH))
+    print("[ SPACE ] Pause".center(WIDTH))
+    print("[ R ] Resume".center(WIDTH))
+    print("[ S ] Stop".center(WIDTH))
+    print("[ Q ] Quit".center(WIDTH))
+draw_ui()
+
 def lyrics_player():
+    
+    global current_lyric, current_time, progress_bar
 
-    Lyrics = []
-
-    with open("lyrics/song.lrc", encoding="utf-8") as file:
-        for line in file:
-            time_stamp, lyric = line.strip().split("]", 1)
-            time_stamp = time_stamp[1:]
-
-            minutes, seconds = time_stamp.split(":")
-            lyric_time = int(minutes) * 60 + float(seconds)
-
-            Lyrics.append((lyric_time, lyric))
-
+ 
     last_lyric = ""
 
     while True:
         if playing:
 
             current_time = pygame.mixer.music.get_pos() / 1000
+
+        
+
+
 
             current_lyric = ""
 
@@ -45,38 +102,22 @@ def lyrics_player():
                     break
             if current_lyric != last_lyric:
 
-                print("\033[11;1H\033[2K", end="")
-                print(f"♪ {current_lyric} ♪".center(WIDTH))
-
+                print(f"\033[11;1H\033[2K", end="")
+                print(current_lyric.center(WIDTH), end="", flush=True)
                 last_lyric = current_lyric
+
+            show_time= format_time(current_time)
+            print(f"\033[15;1H\033[2K", end="")
+            print(f"{show_time} / {total_time}".center(WIDTH), end="", flush=True)
             time.sleep(0.1)
         
-thread = threading.Thread(target=lyrics_player)
+thread = threading.Thread(target=lyrics_player,daemon=True)
 thread.start()
-def draw_ui():
-    print(r"""
-       ____  _____________.__                              
-  ____ \   \/  /\______   \  | _____  ___.__. ___________  current_lyric
-_/ __ \ \     /  |     ___/  | \__  \<   |  |/ __ \_  __ \ 
-\  ___/ /     \  |    |   |  |__/ __ \\___  \  ___/|  | \/ 
- \___  >___/\  \ |____|   |____(____  / ____|\___  >__|    
-     \/      \_/                    \/\/         \/        
-""")
 
-    print()
-    print("                 LEVEL FIVE - TUMI")
-    print()
-    print()
-    print("              [===================>-----]")
-    print("                    01:24 / 03:42")
-    print()
-    print("              [ P ] Play")
-    print("              [ SPACE ] Pause")
-    print("              [ R ] Resume")
-    print("              [ S ] Stop")
-    print("              [ Q ] Quit")
-    
-draw_ui()
+
+
+
+
 
 while True:
 
